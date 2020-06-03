650de3adc6d361830ad1c047993ea61be0fe4937
Alexandre.BDO <60094600+bdoalex@users.noreply.github.com>
2020-06-03 11:35:19 +0200

    serveur crée en local + début du back

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var router = express.Router();
-router.get('/', function (req, res, next) {
-    res.send("API is working properly");
-});
-module.exports = router;
+var async = require('express-async-await')
+var fetch = require('node-fetch')
+
+const GoogleKey = "AIzaSyCDa_5CfpTTBeNY0PFremSA5i_1zqaGnEU"
+
+router.get('/', async function (req, res, next) {
+    function ooIfoundData() {
+        res.header("Access-Control-Allow-Origin","*")
+        var address = req.address
+        return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GoogleKey}`)
+
+    }
+
+    const ooIprocessData = async () => {
+        const github = await ooIfoundData()
+        const ooiResponseData = await github.json()
+        res.send(ooiResponseData)
+    }
+    ooIprocessData()
+    res.end
+
+})
+
+module.exports = router
