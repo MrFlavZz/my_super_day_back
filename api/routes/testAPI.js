4ec648987984772fef9ac0b5d141673ba1b6160e
Alexandre.BDO <60094600+bdoalex@users.noreply.github.com>
2020-06-03 16:17:44 +0200

    création APi personnalisée

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,22 +5,19 @@ var fetch = require('node-fetch')
 
 const GoogleKey = "AIzaSyCDa_5CfpTTBeNY0PFremSA5i_1zqaGnEU"
 
-router.get('/', async function (req, res, next) {
+router.get('/',async function (req, res, next) {
+   let address=req.param("address");
     function ooIfoundData() {
-        res.header("Access-Control-Allow-Origin","*")
-        var address = req.address
         return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GoogleKey}`)
-
     }
-
     const ooIprocessData = async () => {
         const github = await ooIfoundData()
         const ooiResponseData = await github.json()
-        res.send(ooiResponseData)
+        let data = (ooiResponseData.results[0].geometry.location)
+        res.send(data)
     }
     ooIprocessData()
     res.end
-
 })
 
 module.exports = router
