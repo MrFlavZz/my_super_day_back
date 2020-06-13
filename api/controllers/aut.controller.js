9071375d928370747e88a0a825748421068df498
MrFlavZz <flav621@gmail.com>
2020-06-13 23:53:12 +0200

    token during one hour

--- api/controllers/aut.controller.js
+++ api/controllers/aut.controller.js
@@ -45,7 +45,7 @@ exports.signin = (req, res) => {
             }
 
             var token = jwt.sign({id: user.id}, config.secret, {
-                expiresIn: 86400 // 24 hours
+                expiresIn: 3600 //1 heure
             });
 
 
